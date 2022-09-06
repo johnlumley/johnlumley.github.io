43580b30caf897a15d264b7c2e075cebd3329942
John Lumley <john@saxonica.com>
2022-09-06 12:03:56 +0100

    V 0.9.8 upload
    Local file change fixed; record processing added

--- fileReader.js
+++ fileReader.js
@@ -1,31 +1,16 @@
-//const fileInput = document.querySelector("input[type=file]");
-//const output = document.querySelector('.output');
-/*const fileInput = document.getElementById('file');
-const outputX = document.getElementById('output');
-console.log("fileInput:", fileInput);
-console.log("output", outputX);
-fileInput.addEventListener("change", () => {
-   const[file] = fileInput.files;
-   if (file) {
-      const reader = new FileReader();
-      reader.addEventListener("load", () => {
-         outputX.innerText = reader.result;
-      });
-      reader.readAsText(file);
-   }
-})
-*/
+/* A simple registration function to attach a file choice.reader to a textarea */
 function addFileReader(input, output) {
    const fileX = document.getElementById(input);
    const out  = document.getElementById(output);
-  /* console.log("fileX:", fileX);
-   console.log("out", out);*/
    fileX.addEventListener("change", () => {
       const[file] = fileX.files;
+      fileX.value = ""; /*  Forces a subsequent read if no change in file chosen */
       if (file) {
          const reader = new FileReader();
          reader.addEventListener("load", () => {
             out.value = reader.result;
+            /*const evt = new InputEvent("input");
+            out.dispatchEvent(evt);*/
          });
          reader.readAsText(file);
       }
