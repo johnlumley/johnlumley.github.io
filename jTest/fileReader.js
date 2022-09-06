f0d9a23d98b338f640701bf98c76e3dc57d21fa7
John Lumley <john@saxonica.com>
2022-09-06 11:58:05 +0100

    V 0.9.8 upload
    Fixed local file change; added record processing

--- jTest/fileReader.js
+++ jTest/fileReader.js
@@ -4,10 +4,13 @@ function addFileReader(input, output) {
    const out  = document.getElementById(output);
    fileX.addEventListener("change", () => {
       const[file] = fileX.files;
+      fileX.value = ""; /*  Forces a subsequent read if no change in file chosen */
       if (file) {
          const reader = new FileReader();
          reader.addEventListener("load", () => {
             out.value = reader.result;
+            /*const evt = new InputEvent("input");
+            out.dispatchEvent(evt);*/
          });
          reader.readAsText(file);
       }
