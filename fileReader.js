01e3f999823f0e4815acb951b50747ac45c7f810
John Lumley <john@saxonica.com>
2022-10-10 10:54:43 +0100

    Upload of V1.0 workbench

--- fileReader.js
+++ fileReader.js
@@ -1,8 +1,12 @@
+function addFileCollect(input, output) {
+   addFileReader(input, output);
+   addDrag(input, output);
+}
 /* A simple registration function to attach a file choice.reader to a textarea */
 function addFileReader(input, output) {
    const fileX = document.getElementById(input);
-   const out  = document.getElementById(output);
-   const fileName  = document.getElementById(input+"Name");
+   const out = document.getElementById(output);
+   const fileName = document.getElementById(input + "Name");
    fileX.addEventListener("change", () => {
       const[file] = fileX.files;
       fileX.value = ""; /*  Forces a subsequent read if no change in file chosen */
@@ -11,12 +15,41 @@ function addFileReader(input, output) {
          const reader = new FileReader();
          reader.addEventListener("load", () => {
             out.value = reader.result;
-            /*const evt = new InputEvent("input");
-            out.dispatchEvent(evt);*/
          });
          reader.readAsText(file);
       } else {
          fileName.textContent = "";
       }
    })
+}
+
+function addDrag(input, output) {
+   const fileName = document.getElementById(input + "Name");
+   const out = document.getElementById(output)
+   out.addEventListener("dragenter", (event) => {
+      out.style.background = "lavenderblush";
+      const img = new Image();
+      img.src = "example.png";
+      event.dataTransfer.setDragImage(img, 20, 20);
+      event.preventDefault();
+   });
+   out.addEventListener("dragleave", (event) => {
+      out.style.background = "white";
+       event.preventDefault();
+   });
+   out.addEventListener("drop", (event) => {[...event.dataTransfer.items].forEach((item, i) => {
+         // If dropped items aren't files, reject them
+         if (item.kind === 'file') {
+            const file = item.getAsFile();
+            fileName.textContent = file.name;
+            const reader = new FileReader();
+            reader.addEventListener("load", () => {
+               out.value = reader.result;
+            });
+            reader.readAsText(file);
+         }
+      });
+      out.style.background = "white";
+      event.preventDefault();
+   });
 }
