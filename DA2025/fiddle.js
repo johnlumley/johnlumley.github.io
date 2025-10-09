da683c9fddd6dcc87c449c3fe6ddbac15609ef70
John Lumley <john@saxonica.com>
2025-10-09 15:02:58 +0100

    Support for different context types

--- DA2025/fiddle.js
+++ DA2025/fiddle.js
@@ -1,10 +1,10 @@
 
-async function baseX(query, context, resultPlace/*, place*/) {
+async function baseX(query, context, contextType, resultPlace) {
    const q = "(" + query + ") => serialize({'method':'adaptive'})";
    const d = new FormData();
    d.append('query', query);
    d.append('context', context);
-   d.append('contextContentType', 'text');
+   d.append('contextContentType', contextType);
    let p = fetch(
    'https://fiddle.basex.org/api/xquery', {
       method: 'POST',
@@ -13,14 +13,18 @@ async function baseX(query, context, resultPlace/*, place*/) {
    p.then((txt) => {
       const t = txt.replaceAll('""', "'");
       const j = JSON.parse(t);
-      results(j.result,resultPlace);
+      if (j.result) {
+         results(j.result, resultPlace);
+      } else {
+         baseXError(j.description, resultPlace);
+      }
    })
 }
 
 function results(input, place) {
    place.replaceChildren();
    let r = document.createElement("pre");
-   r.textContent = input.length + " item" + (input.length != 1 ? "s" : "");
+   r.textContent = input.length + " item" + (input.length != 1 ? "s": "");
    place.append(r);
    input.forEach((item) => {
       r = document.createElement("pre");
@@ -28,20 +32,26 @@ function results(input, place) {
       place.append(r);
    })
 }
-async function baseX2(query, context, result) {
-   /*const result = document.getElementById(resultID);*/
-   const q = "(" + query + ") => serialize({'method':'json'})";
-   const d = new FormData();
-   d.append('query', query);
-   d.append('context', context);
-   d.append('contextContentType', 'text');
-   let p = fetch(
-   'https://fiddle.basex.org/api/xquery', {
-      method: 'POST',
-      body: d,
-   }).then((response) => response.json());
-   p.then((json) => result.textContent = json.result);
+function baseXError(input, place) {
+   place.replaceChildren();
+   let r = document.createElement("pre");   
+   r.setAttribute("class", "error");
+   r.textContent = "Error: " + input;
+   place.append(r);
 }
+/*async function baseX2(query, context, contextType, result) {
+ const q = "(" + query + ") => serialize({'method':'json'})";
+ const d = new FormData();
+ d.append('query', query);
+ d.append('context', context);
+ d.append('contextContentType', 'text');
+ let p = fetch(
+ 'https://fiddle.basex.org/api/xquery', {
+ method: 'POST',
+ body: d,
+ }).then((response) => response.json());
+ p.then((json) => result.textContent = json.result);
+ }*/
 async function setClipboard(text) {
    const type = "text/plain";
    const clipboardItemData = {[type]: text,
