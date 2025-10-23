ac5163c302c4f785ebcacd094a827b2f3d277165
John Lumley <john@saxonica.com>
2025-10-23 15:33:16 +0100

    Detecting fiddle limits; adding problem statements

--- DA2025/fiddle.js
+++ DA2025/fiddle.js
@@ -9,10 +9,8 @@ async function baseX(query, context, contextType, resultPlace) {
    'https://fiddle.basex.org/api/xquery', {
       method: 'POST',
       body: d,
-   }).then((response) => response.text());
-   p.then((txt) => {
-      const t = txt.replaceAll('""', "'");
-      const j = JSON.parse(t);
+   }).then((response) => response.json());
+   p.then((j) => {
       if (j.result) {
          results(j.result, resultPlace);
       } else {
@@ -24,34 +22,33 @@ async function baseX(query, context, contextType, resultPlace) {
 function results(input, place) {
    place.replaceChildren();
    let r = document.createElement("pre");
-   r.textContent = input.length + " item" + (input.length != 1 ? "s": "");
+   if (input.length == 100) {
+      r.setAttribute("class", "caution");
+      r.textContent = "Warning: result display limited to first 100 items";
+   } else {
+      r.textContent = input.length + " item" + (input.length != 1 ? "s": "")
+   }
    place.append(r);
    input.forEach((item) => {
       r = document.createElement("pre");
-      r.textContent = item.replaceAll('&#xA;', '\n');
+      if (item.length >= 10000) {
+         const warn = document.createElement("pre");
+         warn.setAttribute("class", "caution");
+         warn.textContent = "Warning: item textual value truncated to 10kB\n";
+         r.append(warn);
+      }
+      r.append(new Text(item.replaceAll('&#xA;', '\n')));
       place.append(r);
    })
 }
 function baseXError(input, place) {
    place.replaceChildren();
-   let r = document.createElement("pre");   
+   let r = document.createElement("pre");
    r.setAttribute("class", "error");
    r.textContent = "Error: " + input;
    place.append(r);
 }
-/*async function baseX2(query, context, contextType, result) {
- const q = "(" + query + ") => serialize({'method':'json'})";
- const d = new FormData();
- d.append('query', query);
- d.append('context', context);
- d.append('contextContentType', 'text');
- let p = fetch(
- 'https://fiddle.basex.org/api/xquery', {
- method: 'POST',
- body: d,
- }).then((response) => response.json());
- p.then((json) => result.textContent = json.result);
- }*/
+
 async function setClipboard(text) {
    const type = "text/plain";
    const clipboardItemData = {[type]: text,
---
+++ DA2025/xslt/structure/test3.4.xsl
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" fixed-namespaces="#default"
+   exclude-result-prefixes="#all" version="4.0">
+   <xsl:mode on-no-match="shallow-copy">
+      <xsl:template match="/"/>
+   </xsl:mode>
+</xsl:stylesheet>
